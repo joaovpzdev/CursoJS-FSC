e12011c7cf3cd2e99e195bec35f04491793c5d1e
joaovpzdev <joaovpz.dev@gmail.com>
2026-07-08 15:56:34 -0300

    how to modified items with JS

---
+++ exercicio4/main.js
@@ -0,0 +1,15 @@
+const items = document.querySelector (".items");
+const button = document.querySelector (".btn");
+//items.remove();
+//removi os itens 
+
+//console.log(items.firstElementChild.remove());
+//removi apenas o primeiro item, porem antes testei com console.log(items.firstElementChild).
+
+//items.firstElementChild.remove();
+//items.lastElementChild.remove();
+//items.children[0].textContent = "Item Americano";
+
+//items.lastElementChild.innerHTML = "<h3>Hello world!</h3>";
+
+button.style.background = "red";
