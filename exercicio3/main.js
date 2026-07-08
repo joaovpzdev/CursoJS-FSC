21eb10429533ffbde8961700818b37078bdbf241
joaovpzdev <joaovpz.dev@gmail.com>
2026-07-08 15:31:02 -0300

    how to acess the DOM

---
+++ exercicio3/main.js
@@ -0,0 +1,17 @@
+//const addUserText = document.getElementById("add-user");
+//console.log(addUserText);
+//usei o console.log para checar se estava pegando o elemento certo, e estava.
+//addUserText.innerText = "Adicionar Usuário"
+//usei esse '.innerText =' para mudar o texto do local que selecionei.
+//const addUserText = document.querySelector ("#add-user")
+//console.log(addUserText)
+//const allItems = document.querySelectorAll (".item");
+//console.log(allItems)
+//serve para selecionar multiplos elementos e etc.
+
+//const allItems = document.querySelectorAll (".items .item")
+//const allItems = document.getElementsByClassName ("item")
+//não é tao bom quanto o queryselectorall.
+
+//const allItems = document.getElementsByTagName ("li");
+//nao é tão bom quanto o queryselectorall.
